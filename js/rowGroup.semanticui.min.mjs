2aec925edf9e686d4a74b80e6fc1a8c3acce9273
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 17:15:37 +0000

    b089337101a134b448651ef1c1b0d75822424f4f
    Sync to source repo @b089337101a134b448651ef1c1b0d75822424f4f

--- js/rowGroup.semanticui.min.mjs
+++ js/rowGroup.semanticui.min.mjs
@@ -1,1 +1,4 @@
+/*! Semanic UI styling wrapper for RowGroup
+ * © SpryMedia Ltd - datatables.net/license
+ */
 import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-rowgroup";export default DataTable;
