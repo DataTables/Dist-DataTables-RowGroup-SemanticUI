96bdd1e8ba54ecffef478fca9d13fc24851b68d3
dtbuild <dtbuild@rigil>
2026-09-25 13:09:18 +0000

    9ac514fee19651bdf0d2431547dc11b18b54ecf8 Release 2.1.1
    Sync to source repo @9ac514fee19651bdf0d2431547dc11b18b54ecf8

--- js/rowGroup.semanticui.min.mjs
+++ js/rowGroup.semanticui.min.mjs
@@ -1,4 +1,4 @@
-/*! RowGroup Fomantic styling 2.1.0 for DataTables
+/*! RowGroup Fomantic styling 2.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-se";import RowGroup from"datatables.net-rowgroup";export default DataTable;
